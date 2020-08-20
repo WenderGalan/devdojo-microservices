c543d33ea7a17c02c04dd07584a3657bbb01d345
Wender Galan <wendergalan2014@hotmail.com>
2020-08-19 20:11:37 -0400

    Aula 3 - Finalizando a criação do gateway e o discovery

---
+++ discovery/src/test/java/wendergalan/github/io/discovery/DiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package wendergalan.github.io.discovery;
+
+import org.junit.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
